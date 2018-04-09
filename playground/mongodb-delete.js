408254d907ad957e62f5b27da0953ba2c154c26b
eric-dl <31330964+eric-dl@users.noreply.github.com>
2018-04-09 07:32:59 -0500

    4/8

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,18 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
+    if (err) {
+        return console.log('unable to connect to mongodb server');
+    }
+    console.log('connected to mongodb server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find().count().then((count)=>{
+    //     console.log(count);
+    // },(err)=>{
+    //     console.log('Error', err);
+    // })
+
+
+    client.close();
+});
